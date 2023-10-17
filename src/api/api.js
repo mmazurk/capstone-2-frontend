2f8616bf4dd3c0b37bfb03c57ec41ae2894d5a05
markmazurk <markmazurk@gmail.com>
2023-10-17 11:59:59 -0700

    test

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
-const BASE_URL = process.env.BASE_API_URL || "http://localhost:3001";
+const BASE_URL = process.env.BASE_API_URL || "http://localhost:3055";
 
 
 class MyPhotoAPI {
